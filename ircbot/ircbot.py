319fc1677556b6df49bcf8cd2756daf535c83224
SteveArcher <zy911004@gmail.com>
2014-08-31 23:24:47 +0800

    Update ircbot.py

--- ircbot/ircbot.py
+++ ircbot/ircbot.py
@@ -6,29 +6,37 @@ reload(sys).setdefaultencoding("utf8")
 
 import socket
 import re
+import time
 
 import chat
 import weather
 import joke
 import jokeuser
 import helpcmd
+import story
 
-irc_network = "irc.freenode.net"
+irc_host = "irc.freenode.net"
 irc_chan = "#linuxba"
 bot_name = "bzzzt"
 irc_port = 6666
+irc_leng = 120
 
 filter_map = [
     (chat,      lambda s: re.search(R"PRIVMSG(.+?):\>b (.+)", s).group(2)),
     (joke,      lambda s: re.search(R"PRIVMSG(.+?):\>j(.+)", s).group(2)),   
-    (jokeuser,  lambda s: re.search(R"PRIVMSG(.+?):\>j (.+)", s).group(2)),
+    (jokeuser,  lambda s: re.search(R"PRIVMSG(.+?):\>j\.(.+)", s).group(2)),
     (weather,   lambda s: re.search(R"PRIVMSG(.+?):\>w (.+)", s).group(2)),
-    (helpcmd,   lambda s: re.search(R"PRIVMSG(.+?):\?b(.+)", s).group(2)),
+    (helpcmd,   lambda s: re.search(R"PRIVMSG(.+?):\>\?(.+)", s).group(2)),
+    (story,   lambda s: re.search(R"PRIVMSG(.+?):\>s(.+)", s).group(2)),
 ]
 
+# Creating socket 
 irc_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-irc_sock.connect( (irc_network, irc_port))
 
+# Connecting to the server
+irc_sock.connect((irc_host, irc_port))
+
+# Communicating with server
 irc_sock.send("NICK " + bot_name + "\r\n")
 irc_sock.send("USER bzt bzt bzt :bot\r\n")
 irc_sock.send("JOIN " + irc_chan + "\r\n")
@@ -44,18 +52,34 @@ while True:
 
     for (filter_mod, filter_fun) in filter_map: ### try matching with each filter
         try:
-            nickname = re.search(R"^(.+?)!", data).group(1)
+            nickname = re.match(r"^:([^!]+)", data).group(1)
             content = filter_fun(data)
             replies = filter_mod.reply(content)
 
-            print "<<< " + content
-            for reply in replies.strip().split("\n"):
-                irc_sock.send("PRIVMSG " + irc_chan + " %s: %s\r\n" % (nickname, reply))
-                print ">>> " + reply
-            break
+            print "<<< " + nickname + ": " + content
+            if len(replies) > irc_leng:
+                head = 0;
+                tail = irc_leng;
+                while (replies[tail]):
+                    while(ord(replies[tail]) & 0xc0 != 0x80):
+                        tail = tail + 1
+
+                    irc_sock.send("PRIVMSG " + irc_chan + " :"  + "%s: %s\r\n" % (nickname, replies[head:tail]))
+                    print ">>> " + replies[head:tail]
+                    time.sleep(1)
+                    head = tail
+                    tail = tail + irc_leng
+                    if tail > len(replies):
+                        irc_sock.send("PRIVMSG " + irc_chan + " :"  + "%s: %s\r\n" % (nickname, replies[head:len(replies)]))
+                        print ">>> " + replies[head:len(replies)]
+
+            else:
+                for reply in replies.strip().split("\n"):
+                    irc_sock.send("PRIVMSG " + irc_chan + " :"  + "%s: %s\r\n" % (nickname, reply))
+                    print ">>> " + reply
 
         except Exception as e:
             pass
 
     else:
-        print ":::" + data.strip()
+        print data.strip()
